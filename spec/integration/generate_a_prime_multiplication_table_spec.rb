490909bb0739b48f1a9240452771b568d5200b57
Ben Lovell <benjamin.lovell@gmail.com>
2015-02-14 20:39:53 +0000

    Corrects expected structure of formatted grid

--- spec/integration/generate_a_prime_multiplication_table_spec.rb
+++ spec/integration/generate_a_prime_multiplication_table_spec.rb
@@ -3,10 +3,10 @@ require_relative '../../lib/prime_table'
 RSpec.describe PrimeTable::PrimeMultiplicationTable do
   let(:table) do
 <<-table
-'   2  3  5'
-'2  4  6 10'
-'3  6  9 15'
-'5 10 15 25'
+     2  3  5
+  2  4  6 10
+  3  6  9 15
+  5 10 15 25
 table
   end
 
