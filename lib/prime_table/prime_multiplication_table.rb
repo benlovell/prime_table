e1e51378ff11665b0b428408c3cb0cf269b42f2f
Ben Lovell <benjamin.lovell@gmail.com>
2015-02-14 18:50:19 +0000

    Gets the multiplication table working (shameless)
    Just committing here after a red/green.
    
    Refactor in 3..2..1..

--- lib/prime_table/prime_multiplication_table.rb
+++ lib/prime_table/prime_multiplication_table.rb
@@ -4,6 +4,26 @@ module PrimeTable
       @primes = PrimeGenerator.generate(number_of_primes)
     end
 
+    def table
+      @table ||= begin
+        Array.new(@primes.count + 1).map { Array.new(@primes.count + 1) }.tap do |grid|
+          grid.each_with_index do |row, row_index|
+            row.each_with_index do |cell, column_index|
+              next if row_index == 0 && column_index == 0
+
+              if row_index == 0
+                grid[row_index][column_index] = @primes[column_index - 1]
+              elsif column_index == 0
+                grid[row_index][column_index] = @primes[row_index - 1]
+              else
+                grid[row_index][column_index] = @primes[column_index - 1] * @primes[row_index - 1]
+              end
+            end
+          end
+        end
+      end
+    end
+
     def print
     end
   end
--- spec/prime_table/prime_multiplication_table_spec.rb
+++ spec/prime_table/prime_multiplication_table_spec.rb
@@ -8,4 +8,21 @@ RSpec.describe PrimeTable::PrimeMultiplicationTable do
       described_class.new(3)
     end
   end
+
+  describe '#table' do
+    before do
+      allow(PrimeTable::PrimeGenerator).to receive(:generate).with(3) { [2, 3, 5] }
+    end
+
+    subject { described_class.new(3).table }
+
+    it 'returns a two dimensional array of factored primes' do
+      expect(subject).to contain_exactly(
+        [nil, 2, 3, 5],
+        [2, 4, 6, 10],
+        [3, 6, 9, 15],
+        [5, 10, 15, 25]
+      )
+    end
+  end
 end
