a06bcdf677fd74fa1cd5944c3930ec747809acb4
Ben Lovell <benjamin.lovell@gmail.com>
2015-02-15 01:11:15 +0000

    Adds a simple script for execution

--- lib/prime_table.rb
+++ lib/prime_table.rb
@@ -1,6 +1,6 @@
-require 'prime_table/printer'
-require 'prime_table/prime_generator'
-require 'prime_table/prime_multiplication_table'
+require_relative 'prime_table/printer'
+require_relative 'prime_table/prime_generator'
+require_relative 'prime_table/prime_multiplication_table'
 
 module PrimeTable
 end
