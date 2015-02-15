3112c246abd7e52b0908a57859d9135316345d80
Ben Lovell <benjamin.lovell@gmail.com>
2015-02-15 18:50:06 +0000

    Moves logic to determine widest cell

--- lib/prime_table/printer.rb
+++ lib/prime_table/printer.rb
@@ -1,13 +1,19 @@
 module PrimeTable
   class Printer
     def self.print(table)
-      padding = table.max_by { |c| c.map(&:to_i) }.max.to_s.length + 1
+      padding = widest_cell_length(table)
 
       output  = table.map do |row|
         row.map { |cell| "%#{padding}s" % cell }.join
       end.join("\n")
 
       puts output
+    end
+
+    private
+
+    def self.widest_cell_length(table)
+      table.max_by { |c| c.map(&:to_i) }.max.to_s.length + 1
     end
   end
 end
