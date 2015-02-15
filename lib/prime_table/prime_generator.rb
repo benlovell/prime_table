549d00d42771716b6d182e129022eb03c4afbfa9
Ben Lovell <benjamin.lovell@gmail.com>
2015-02-15 00:36:25 +0000

    Adds a naive prime generator
    I purposefully avoided lifting a sieve from somewhere, as I'd forgotten
    the exact algorithm. This version is kinda slow but it would be nice
    given the time, to created a threaded version. Or, just drop in a
    proper sieve ;)

--- lib/prime_table/prime_generator.rb
+++ lib/prime_table/prime_generator.rb
@@ -1,7 +1,20 @@
 module PrimeTable
   class PrimeGenerator
     def self.generate(number_of_primes)
-      [2, 3, 5]
+      [2].tap do |primes|
+        counter = 1
+
+        until primes.count == number_of_primes
+          number = primes.last + counter
+
+          if (2...number).any? { |i| number % i == 0 }
+            counter += 1
+          else
+            primes << number
+            counter = 1
+          end
+        end
+      end
     end
   end
 end
